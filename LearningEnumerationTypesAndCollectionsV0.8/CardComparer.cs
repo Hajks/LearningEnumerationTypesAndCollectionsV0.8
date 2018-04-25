58e0a47d6fc3c5796cef844d7482f8d2e0972b35
Hajk <tunczorx@gmail.com>
2018-04-25 10:59:41 +0200

    Five random cards, sorted by IComparer. Working well.

---
+++ LearningEnumerationTypesAndCollectionsV0.8/CardComparer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningEnumerationTypesAndCollectionsV0._8
+{
+    public enum SortCriteria
+    {
+        ValueThenType,
+        TypeThenValue,
+    }
+    class CardComparer : IComparer<Card>
+    {
+        public SortCriteria SortBy = SortCriteria.ValueThenType;
+        public int Compare(Card x, Card y)
+        {
+            if (SortBy == SortCriteria.ValueThenType)
+                if (x.Value > y.Value)
+                    return 1;
+                else if (x.Value < y.Value)
+                    return -1;
+                else
+                    if (x.Type > y.Type)
+                        return 1;
+                    else if (x.Type < y.Type)
+                        return -1;
+                    else
+                        return 0;
+            else
+                if (x.Type > y.Type)
+                    return 1;
+                else if (x.Type < y.Type)
+                    return -1;
+                if (x.Value > y.Value)
+                    return 1;
+                else if (x.Value < y.Value)
+                    return -1;
+                else
+                    return 0;
+        }
+    }
+}
--- LearningEnumerationTypesAndCollectionsV0.8/Program.cs
+++ LearningEnumerationTypesAndCollectionsV0.8/Program.cs
@@ -10,6 +10,37 @@ namespace LearningEnumerationTypesAndCollectionsV0._8
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            List<Card> cards = new List<Card>();
+            /*
+            {   
+                 //code, before optimalization
+
+                new Card() { Type = (Types)random.Next(0,3), Value =(Values)random.Next(1,13)},
+                new Card() { Type = (Types)random.Next(0,3), Value =(Values)random.Next(1,13)},
+                new Card() { Type = (Types)random.Next(0,3), Value =(Values)random.Next(1,13)},
+                new Card() { Type = (Types)random.Next(0,3), Value =(Values)random.Next(1,13)},
+                new Card() { Type = (Types)random.Next(0,3), Value =(Values)random.Next(1,13)},
+              
+            };
+              */
+            Console.WriteLine("Przed posortowaniem:");
+            for (int i = 0; i < 5; i++)
+            {
+                cards.Add(new Card((Types)random.Next(4), (Values)random.Next(1, 13)));
+                Console.WriteLine(cards[i].Name);
+            }
+
+            Console.WriteLine("\r\n"+"Po posortowaniu:");
+            CardComparer cardComparer = new CardComparer();
+            cardComparer.SortBy = SortCriteria.TypeThenValue; // Here we can decide how we want to sor our values.
+            cards.Sort(cardComparer);
+            foreach(Card card in cards)
+            {
+                Console.WriteLine(card.Name);
+            }
+            Console.ReadKey();
         }
+
     }
 }
